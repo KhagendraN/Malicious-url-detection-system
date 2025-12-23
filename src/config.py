31185e68655e5b3ebcd6069a04dd96e6bccd73bb
KhagendraN <nkhagendra1@gmail.com>
2025-12-23 19:34:23 +0545

    core: add configuration settings

---
+++ src/config.py
@@ -0,0 +1,7 @@
+import os
+
+RAW_DATA_PATH = os.path.join("data", "raw", "malicious_phish.csv")
+PROCESSED_DATA_PATH = os.path.join("data", "processed", "malicious_phish_featured.csv")
+MODEL_PATH = "models"
+RANDOM_SEED = 42
+REPORT_PATH = "reports"
